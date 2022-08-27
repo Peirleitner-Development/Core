d67ec2ff33f5591fe0c300c30fae649d1b3e47f4
Markus Peirleitner <markus@peirleitner.at>
2022-08-27 18:35:11 +0200

    added base64 utils

--- src/at/peirleitner/core/util/local/LocalUtils.java
+++ src/at/peirleitner/core/util/local/LocalUtils.java
@@ -5,7 +5,14 @@ import java.util.UUID;
 import javax.annotation.Nonnull;
 
 import org.bukkit.Location;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.io.BukkitObjectInputStream;
+import org.bukkit.util.io.BukkitObjectOutputStream;
+import org.yaml.snakeyaml.external.biz.base64Coder.Base64Coder;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -59,6 +66,45 @@ public class LocalUtils {
 		float pitch = location.getPitch();
 		
 		return new CustomLocation(worldName, x, y, z, yaw, pitch);
+	}
+	
+	public static String itemStackArrayToBase64(ItemStack[] items) throws IllegalStateException {
+		try {
+			ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+			BukkitObjectOutputStream dataOutput = new BukkitObjectOutputStream(outputStream);
+
+			// Write the size of the inventory
+			dataOutput.writeInt(items.length);
+
+			// Save every element in the list
+			for (int i = 0; i < items.length; i++) {
+				dataOutput.writeObject(items[i]);
+			}
+
+			// Serialize that array
+			dataOutput.close();
+			return Base64Coder.encodeLines(outputStream.toByteArray());
+		} catch (Exception e) {
+			throw new IllegalStateException("Unable to save item stacks.", e);
+		}
+	}
+	
+	public static ItemStack[] itemStackArrayFromBase64(String data) throws IOException {
+		try {
+			ByteArrayInputStream inputStream = new ByteArrayInputStream(Base64Coder.decodeLines(data));
+			BukkitObjectInputStream dataInput = new BukkitObjectInputStream(inputStream);
+			ItemStack[] items = new ItemStack[dataInput.readInt()];
+
+			// Read the serialized inventory
+			for (int i = 0; i < items.length; i++) {
+				items[i] = (ItemStack) dataInput.readObject();
+			}
+
+			dataInput.close();
+			return items;
+		} catch (ClassNotFoundException e) {
+			throw new IOException("Unable to decode class type.", e);
+		}
 	}
 	
 }
