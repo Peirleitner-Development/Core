8955eb055ee3044363d2ee976939ddc6e8f54c97
Markus Peirleitner <markus@peirleitner.at>
2022-09-15 15:47:06 +0200

    More changed towards the GMS

--- src/at/peirleitner/core/system/GameMapSystem.java
+++ src/at/peirleitner/core/system/GameMapSystem.java
@@ -40,56 +40,49 @@ public class GameMapSystem {
 	}
 
 	private final void loadMaps() {
-		
-		if(!this.isMapCachingEnabled()) {
-			Core.getInstance().log(this.getClass(), LogType.DEBUG, "Did not load Maps because caching has been disabled.");
+
+		if (!this.isMapCachingEnabled()) {
+			Core.getInstance().log(this.getClass(), LogType.DEBUG,
+					"Did not load Maps because caching has been disabled.");
 			return;
 		}
-		
+
 		try {
-			
-			PreparedStatement stmt = Core.getInstance().getMySQL().getConnection().prepareStatement("SELECT * FROM " + this.table);
+
+			PreparedStatement stmt = Core.getInstance().getMySQL().getConnection()
+					.prepareStatement("SELECT * FROM " + this.table);
 			ResultSet rs = stmt.executeQuery();
-			
-			while(rs.next()) {
-				
+
+			while (rs.next()) {
+
 				GameMap map = this.getByResultSet(rs);
 				this.cachedMaps.add(map);
-				
+
 			}
-			
-			Core.getInstance().log(this.getClass(), LogType.INFO, "Cached " + this.cachedMaps.size() + " Maps from Database.");
-			
+
+			Core.getInstance().log(this.getClass(), LogType.INFO,
+					"Cached " + this.cachedMaps.size() + " Maps from Database.");
+
 		} catch (SQLException e) {
-			Core.getInstance().log(this.getClass(), LogType.ERROR, "Could not load Maps from Database/SQL: " + e.getMessage());
+			Core.getInstance().log(this.getClass(), LogType.ERROR,
+					"Could not load Maps from Database/SQL: " + e.getMessage());
 			return;
 		}
-		
+
 	}
-	
+
 	public final boolean isMapCachingEnabled() {
 		return Core.getInstance().getSettingsManager().isSetting(Core.getInstance().getPluginName(), "cache-game-maps");
 	}
 
-	private final GameMap getFromCache(@Nonnull int id) {
-		return this.cachedMaps.stream().filter(map -> map.getID() == id).findAny().orElse(null);
-	}
-	
 	public final GameMap getMap(@Nonnull int id) {
-		return this.getFromCache(id) == null ? this.getFromDatabase(id) : this.getFromCache(id);
-	}
 
-	private final void cache(@Nonnull GameMap map) {
+		GameMap map = this.cachedMaps.stream().filter(m -> m.getID() == id).findAny().orElse(null);
 
-		if (!this.isMapCachingEnabled())
-			return;
-		if (this.cachedMaps.contains(map))
-			return;
-
-		this.cachedMaps.add(map);
-	}
-
-	private final GameMap getFromDatabase(@Nonnull int id) {
+		// Return cached value if available
+		if (map != null) {
+			return map;
+		}
 
 		try {
 
@@ -100,20 +93,168 @@ public class GameMapSystem {
 			ResultSet rs = stmt.executeQuery();
 
 			if (rs.next()) {
-				GameMap map = this.getByResultSet(rs);
+
+				map = this.getByResultSet(rs);
 				this.cache(map);
 
 				return map;
+
 			} else {
 				return null;
 			}
 
 		} catch (SQLException e) {
-			Core.getInstance().log(this.getClass(), LogType.ERROR,
-					"Could not get GameMap with ID '" + id + "' from Database/SQL: " + e.getMessage());
+			Core.getInstance().log(getClass(), LogType.ERROR,
+					"Could not get Map with ID '" + id + "' from Database/SQL: " + e.getMessage());
 			return null;
 		}
 
+	}
+
+	public final boolean isMap(@Nonnull int id) {
+		return this.getMap(id) == null ? false : true;
+	}
+
+	public final GameMap getMap(@Nonnull String name, @Nonnull SaveType saveType) {
+
+		GameMap map = this.cachedMaps.stream()
+				.filter(m -> m.getName().equalsIgnoreCase(name) && m.getSaveType().getID() == saveType.getID())
+				.findAny().orElse(null);
+
+		// Return cached value if available
+		if (map != null) {
+			return map;
+		}
+
+		try {
+
+			PreparedStatement stmt = Core.getInstance().getMySQL().getConnection()
+					.prepareStatement("SELECT * FROM " + this.table + " WHERE name = ? AND saveType = ?");
+			stmt.setString(1, name);
+			stmt.setInt(2, saveType.getID());
+
+			ResultSet rs = stmt.executeQuery();
+
+			if (rs.next()) {
+
+				map = this.getByResultSet(rs);
+				this.cache(map);
+
+				return map;
+
+			} else {
+				return null;
+			}
+
+		} catch (SQLException e) {
+			Core.getInstance().log(getClass(), LogType.ERROR, "Could not get Map with name '" + name
+					+ "' and SaveType '" + saveType.getID() + "' from Database/SQL: " + e.getMessage());
+			return null;
+		}
+
+	}
+
+	public final boolean isMap(@Nonnull String name, @Nonnull SaveType saveType) {
+		return this.getMap(name, saveType) == null ? false : true;
+	}
+
+	/**
+	 * 
+	 * @param uuid - UUID
+	 * @return All Maps where either the {@link GameMap#getCreator()} or
+	 *         {@link GameMap#getContributors()} contains the given UUID
+	 * @since 1.0.3
+	 * @author Markus Peirleitner (Rengobli)
+	 */
+	public final Collection<GameMap> getMaps(@Nonnull UUID uuid) {
+
+		Collection<GameMap> maps = new ArrayList<>();
+
+		if (this.isMapCachingEnabled()) {
+
+			for (GameMap gm : this.cachedMaps) {
+				if (gm.getCreator().equals(uuid) || gm.hasContributors() && gm.getContributors().contains(uuid)) {
+					maps.add(gm);
+				}
+			}
+
+			return maps;
+
+		} else {
+
+			try {
+
+				PreparedStatement stmt = Core.getInstance().getMySQL().getConnection().prepareStatement(
+						"SELECT * FROM " + this.table + " WHERE creator = ? OR contributors LIKE '%?%'");
+				stmt.setString(1, uuid.toString());
+				stmt.setString(2, uuid.toString());
+
+				ResultSet rs = stmt.executeQuery();
+
+				while (rs.next()) {
+					maps.add(this.getByResultSet(rs));
+				}
+
+				return maps;
+
+			} catch (SQLException e) {
+				Core.getInstance().log(this.getClass(), LogType.ERROR,
+						"Could not get Maps of User '" + uuid.toString() + "'/SQL: " + e.getMessage());
+				return null;
+			}
+
+		}
+
+	}
+
+	public final Collection<GameMap> getMaps(@Nonnull GameMapState state) {
+
+		Collection<GameMap> maps = new ArrayList<>();
+
+		if (this.isMapCachingEnabled()) {
+
+			for (GameMap gm : this.cachedMaps) {
+				if (gm.getState() == state) {
+					maps.add(gm);
+				}
+			}
+
+			return maps;
+
+		} else {
+
+			try {
+
+				PreparedStatement stmt = Core.getInstance().getMySQL().getConnection()
+						.prepareStatement("SELECT * FROM " + this.table + " WHERE state = ?");
+				stmt.setString(1, state.toString());
+
+				ResultSet rs = stmt.executeQuery();
+
+				while (rs.next()) {
+					maps.add(this.getByResultSet(rs));
+				}
+
+				return maps;
+
+			} catch (SQLException e) {
+				Core.getInstance().log(this.getClass(), LogType.ERROR,
+						"Could not get Maps of State '" + state.toString() + "'/SQL: " + e.getMessage());
+				return null;
+			}
+
+		}
+
+	}
+
+	private final void cache(@Nonnull GameMap map) {
+
+		if (!this.isMapCachingEnabled())
+			return;
+		if (this.cachedMaps.contains(map))
+			return;
+
+		this.cachedMaps.add(map);
 	}
 
 	private final GameMap getByResultSet(@Nonnull ResultSet rs) throws SQLException {
