391245b0b5d52a0bd4319623a281a64cc0de61ef
Markus Peirleitner <markus@peirleitner.at>
2022-09-30 11:12:50 +0200

    Added import option of old json data

--- src/at/peirleitner/core/command/local/CommandCore.java
+++ src/at/peirleitner/core/command/local/CommandCore.java
@@ -1,5 +1,7 @@
 package at.peirleitner.core.command.local;
 
+import java.io.FileReader;
+
 import javax.annotation.Nonnull;
 
 import org.bukkit.command.Command;
@@ -7,8 +9,13 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.PluginDescriptionFile;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import at.peirleitner.core.Core;
 import at.peirleitner.core.SpigotMain;
+import at.peirleitner.core.util.LogType;
 import at.peirleitner.core.util.user.CorePermission;
 import at.peirleitner.core.util.user.Language;
 import net.md_5.bungee.api.ChatColor;
@@ -19,46 +26,71 @@ public class CommandCore implements CommandExecutor {
 		SpigotMain.getInstance().getCommand("core").setExecutor(this);
 	}
 
-	private final String VERSION_OUTPUT = ChatColor.translateAlternateColorCodes('&', "&9{0} &7version &9{1} &7by &9{2}&7. Website&8: &9{3}&7.");
-	
+	private final String VERSION_OUTPUT = ChatColor.translateAlternateColorCodes('&',
+			"&9{0} &7version &9{1} &7by &9{2}&7. Website&8: &9{3}&7.");
+
 	@Override
 	public boolean onCommand(CommandSender cs, Command cmd, String arg, String[] args) {
-		
+
 		// Send version output if the user isn't an admin or the console itself
-		if(args.length == 0 || !cs.hasPermission(CorePermission.COMMAND_CORE_ADMIN.getPermission())) {
-			
+		if (args.length == 0 || !cs.hasPermission(CorePermission.COMMAND_CORE_ADMIN.getPermission())) {
+
 			PluginDescriptionFile pdf = SpigotMain.getInstance().getDescription();
-			
-			cs.sendMessage(Core.getInstance().getLanguageManager().getPrefix(Core.getInstance().getPluginName(), Language.ENGLISH) + this.VERSION_OUTPUT
-					.replace("{0}", pdf.getName())
-					.replace("{1}", pdf.getVersion())
-					.replace("{2}", pdf.getAuthors().get(0))
-					.replace("{3}", pdf.getWebsite())
-					);
+
+			cs.sendMessage(Core.getInstance().getLanguageManager().getPrefix(Core.getInstance().getPluginName(),
+					Language.ENGLISH)
+					+ this.VERSION_OUTPUT.replace("{0}", pdf.getName()).replace("{1}", pdf.getVersion())
+							.replace("{2}", pdf.getAuthors().get(0)).replace("{3}", pdf.getWebsite()));
 			return true;
 		}
-		
+
 		// Admin
-		if(args.length == 1) {
-			
-			if(args[0].equalsIgnoreCase("loadDefaultSaveTypes")) {
+		if (args.length == 1) {
+
+			if (args[0].equalsIgnoreCase("loadDefaultSaveTypes")) {
 				Core.getInstance().createDefaultSaveTypes();
-				cs.sendMessage(Core.getInstance().getLanguageManager().getMessage(Core.getInstance().getPluginName(), Language.ENGLISH, "command.core.loadDefaultSaveTypes.info", null));
+				cs.sendMessage(Core.getInstance().getLanguageManager().getMessage(Core.getInstance().getPluginName(),
+						Language.ENGLISH, "command.core.loadDefaultSaveTypes.info", null));
+				return true;
+			} else if (args[0].equalsIgnoreCase("importOldJsonData")) {
+
+				try {
+					JsonParser parser = new JsonParser();
+					JsonArray a = (JsonArray) parser.parse(new FileReader(Core.getInstance().getDataFolder() + "/players.json"));
+
+					int i=1;
+					for (Object o : a) {
+						JsonObject person = (JsonObject) o;
+
+						String uuid = person.get("uuid").toString();
+						String rank = person.get("rank").toString();
+						long firstJoinTimestamp = person.get("firstJoinTimestamp").getAsLong();
+						long lastQuitTimestamp = person.get("lastQuitTimestamp").getAsLong();
+						
+						Core.getInstance().log(getClass(), LogType.INFO, "Import #" + i + ": [uuid=" + uuid + ",rank=" + rank + ",firstJoinTimestamp=" + firstJoinTimestamp + ",lastQuitTimestamp=" + lastQuitTimestamp + "]");
+						i++;
+						
+					}
+				}catch(Exception ex) {
+					Core.getInstance().log(this.getClass(), LogType.ERROR, "Error on import: " + ex.getMessage());
+				}
+
 				return true;
 			} else {
 				this.sendHelp(cs);
 				return true;
 			}
-			
+
 		} else {
 			this.sendHelp(cs);
 			return true;
 		}
-		
+
 	}
-	
+
 	private final void sendHelp(@Nonnull CommandSender cs) {
-		cs.sendMessage(Core.getInstance().getLanguageManager().getMessage(Core.getInstance().getPluginName(), Language.ENGLISH, "command.core.syntax", null));
+		cs.sendMessage(Core.getInstance().getLanguageManager().getMessage(Core.getInstance().getPluginName(),
+				Language.ENGLISH, "command.core.syntax", null));
 	}
 
 }
